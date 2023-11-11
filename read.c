8f4e8316c42479bf7d81cf38a1671943f0661074
Sarath Chandra Godithi <sarathchandragodithi@gmail.com>
2023-11-11 16:33:09 +0530

    refactor: Change indent width from 2 to 4

--- read.c
+++ read.c
@@ -3,10 +3,10 @@
 #include <string.h>
 
 int open_and_read_file(char *file) {
-  FILE *mp3_file = fopen(file, "r");
-  if (mp3_file == NULL) {
-    perror("fopen error");
-    return -1;
-  }
-  return 1;
+    FILE *mp3_file = fopen(file, "r");
+    if (mp3_file == NULL) {
+        perror("fopen error");
+        return -1;
+    }
+    return 1;
 }
