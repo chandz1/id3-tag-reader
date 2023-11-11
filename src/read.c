53543d6c36d1f28b731d19fd0230fac2980244a3
Sarath Chandra Godithi <sarathchandragodithi@gmail.com>
2023-11-11 15:29:06 +0530

    feat: Add open file functionality

---
+++ src/read.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int open_and_read_file(char *file) {
+  FILE *mp3_file = fopen(file, "r");
+  if (mp3_file == NULL) {
+    perror("fopen error");
+  }
+  return 1;
+}
